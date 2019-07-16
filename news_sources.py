8f35b1c9b5d7ecb7f4a6916e6db348059fb954d6
dray89 <46060603+dray89@users.noreply.github.com>
2019-07-16 08:39:44 -0400

    Update news_sources.py

--- news_sources.py
+++ news_sources.py
@@ -96,6 +96,6 @@ class email(articles):
         s = smtplib.SMTP('smtp.gmail.com', 587)
         s.ehlo()
         s.starttls()
-        s.login('raydebra89@gmail.com', 'qoerbzlkaocdttam')  
+        s.login('raydebra89@gmail.com', 'password')  
         s.send_message(self.compose())
         s.quit()
