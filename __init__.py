58f5e1d1217717c0a551a5604d1c40b0a9841f42
Debra Ray <raydebra89@gmail.com>
2019-10-05 09:56:51 -0400

    update

--- __init__.py
+++ __init__.py
@@ -10,3 +10,9 @@ from newspaper import Article, fulltext
 from GoogleNews import GoogleNews
 import pandas as pd
 import traceback
+from news_updates.news_sources import News
+
+def textfile(self, document_name, index_list):
+    with open(document_name, 'w+') as doc:
+        for each in list(index_list):
+            doc.write(each+'\n')
--- news_sources.py
+++ news_sources.py
@@ -3,99 +3,36 @@
 Created on Sat Jul 13 15:51:42 2019
 ''' sends news updates '''
 @author: rayde
+
 """
-from newspaper import Article, nlp
-from GoogleNews import GoogleNews
-import pandas as pd
-import datetime
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from newspaper import Article
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
 
-class NewsSearch:
-    def __init__(self, excel="C:\\Users\\rayde\\Documents\\CA_motleyfool.xlsx"):
-        self.excel = excel
-        self.error = 0
-        self.success = 0
-        self.bad_links = 'bad_links.txt'
-        self.name = self.file_name()
-        
-    def file_name(self):
-        '''write to file with date in name'''
-        date = datetime.datetime.now()
-        date = date.strftime("%Y-%m-%d")
-        name = 'C:\\Users\\rayde\\newspapers\\{0}_news.txt'
-        return name.format(date)
-    
-class ticker_df(NewsSearch):     
-    def get_df(self):
-        stocks = pd.read_excel(self.excel, usecols=[1])
-        stocks.dropna(inplace=True)
-        stocks = pd.DataFrame(stocks, index= None)
-        return stocks
+class News:
+    def __init__(self, searchterm):
+        self.term = searchterm
+        self.url= "https://news.google.com/rss/search?q=" + searchterm + "&hl=en-CA&gl=CA&ceid=CA%3Aen"
+        self.links = set()
+        self.art_list = []
+        self.GoogleSearch()
 
-    def run_search(self):
-        stocks = self.get_df()
-        googlenews = GoogleNews()
-        links = []
-        for i, j in stocks.itertuples():
-            googlenews.search(j)
-            results = googlenews.getlinks()
-            for link in results:
-                links.append(link)
-        return links
-    
-class articles(ticker_df):
-    def build_articles(self, links):
-        '''create document'''
-        with open(self.name, "w") as document:
-            with open(self.bad_links,'a+') as bad_links:
-                for link in links:
-                    if link not in bad_links: 
-                        try:
-                            article = Article(link)
-                            article.build()
-                            self.content(article, document)
-                            self.success +=1
-                        except:
-                            self.error +=1
-                            links.remove(link)
-                            bad_links.write(link)
-                            continue
-        print(self.count)
-        print(self.x)
-        return links
-        
-    def content(self, article, document):
-        article.nlp()
-        '''create content'''
-        self.title = document.write(article.title)
-        self.authors = document.write(article.authors)
-        self.date = document.write(article.publish_date)
-        self.key = document.write(article.keywords)
-        document.write(article.summary)
-    
-class email(articles):    
-    def read_template(self):
-        filename = self.name
-        with open(filename, 'r', encoding='utf-8') as template_file:
-            template_file_content = template_file.read()
-            return template_file_content
-        
-    def compose(self):
-        message = self.read_template()
-        msg = MIMEMultipart()
-        msg['From'] = 'raydebra89@gmail.com'
-        msg['To'] = 'debraray89@icloud.com'
-        msg['Subject']= 'Your News Update is Ready'
-        msg.attach(MIMEText(message, 'plain'))
-        self.msg = msg
-        return msg
-    
-    def send_email(self):
-        s = smtplib.SMTP('smtp.gmail.com', 587)
-        s.ehlo()
-        s.starttls()
-        s.login('raydebra89@gmail.com', 'password')  
-        s.send_message(self.compose())
-        s.quit()
+    def GoogleSearch(self):
+        Client=urlopen(self.url)
+        xml_page=Client.read()
+        Client.close()
+        soup_page=soup(xml_page,"xml")
+        self.news_list=soup_page.findAll("item")
+        for each in self.news_list:
+            if len(self.links)<20:
+                self.links.add(each.link.text)
+
+        for link in self.links:
+            art = Article(link)
+            try:
+                art.build()
+                art.nlp()
+            except:
+                pass
+            finally:
+                self.art_list.append(art)
