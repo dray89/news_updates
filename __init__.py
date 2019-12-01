887d0950820ada8e5c1c9a084bc588ebd627d20b
Debra Ray <raydebra89@gmail.com>
2019-11-30 21:14:17 -0500

    update

--- __init__.py
+++ __init__.py
@@ -5,12 +5,6 @@ Created on Sat Jul 13 15:54:17 2019
 @author: rayde
 """
 
-import newspaper
-from newspaper import Article, fulltext
-from GoogleNews import GoogleNews
-import pandas as pd
-import traceback
-from news_updates.news_sources import News
 
 def textfile(self, document_name, index_list):
     with open(document_name, 'w+') as doc:
