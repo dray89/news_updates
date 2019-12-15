148d70d1630e5386e0030ad6f258eb80693ed844
Debra Ray <raydebra89@gmail.com>
2019-12-14 21:33:26 -0500

    update

--- news_sources.py
+++ news_sources.py
@@ -10,12 +10,27 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 
 class News:
-    def __init__(self, searchterm):
+    def __init__(self, searchterm, number=20):
+        '''
+        
+
+        Parameters
+        ----------
+        searchterm : Formatted search term (no spaces allowed use -)
+        number : TYPE, optional
+            The number of results the search should yield. The default is 20.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.term = searchterm
         self.url= "https://news.google.com/rss/search?q=" + searchterm + "&hl=en-CA&gl=CA&ceid=CA%3Aen"
         self.links = set()
         self.art_list = []
         self.GoogleSearch()
+        self.number = number
 
     def GoogleSearch(self):
         Client=urlopen(self.url)
@@ -24,7 +39,7 @@ class News:
         soup_page=soup(xml_page,"xml")
         self.news_list=soup_page.findAll("item")
         for each in self.news_list:
-            if len(self.links)<20:
+            if len(self.links)<self.number:
                 self.links.add(each.link.text)
 
         for link in self.links:
--- use_news.py
+++ use_news.py
@@ -11,17 +11,13 @@ def replace(a):
     return a.replace('\\n', " ").replace('\\xe2\\x80\\x99', "'").replace("b'", '')
 
 def y(x):
-    y = (x.title , '*' ,  x.url , '*' ,  x.top_image, '*' ,  str(x.publish_date) , '*' ,  
-    replace(str(x.text.encode())), '*' ,  str(x.authors) , '*' ,  replace(str(x.summary.encode())), '*' ,  x.title, '`')
+    y = (x.title, ' #womenintech #womenwhocode #datascience' ,";",  x.url, '\n')
     return y
             
 if __name__ == "__main__":
-    search = News('nonprofit-coding-bootcamps')
-    article_list = search.art_list
-    columns = ['Title',	'URL', 'Image',	'Date',	'Long Description',	'Reporter Name',
-           'Short Description',	'News (Title)']
-    
+    search = News('nonprofit-coding-bootcamps', 120)
+    article_list = search.art_list    
     b = list(map(lambda each: y(each), article_list))
-    with open('news.txt', 'a') as file:
+    with open('news.txt', 'a', encoding='utf-8') as file:
         for each in b:
             file.writelines(each)
