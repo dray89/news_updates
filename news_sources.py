f0ae286262d68c153a27a782ac5044e0d73dc2b6
Debra Ray <raydebra89@gmail.com>
2021-03-01 19:37:44 -0500

    update

--- news_sources.py
+++ news_sources.py
@@ -8,9 +8,10 @@ Created on Sat Jul 13 15:51:42 2019
 from newspaper import Article
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
+import numpy as np
 
 class News:
-    def __init__(self, searchterm, number=20):
+    def __init__(self, searchterm):
         '''
         
 
@@ -29,7 +30,6 @@ class News:
         self.url= "https://news.google.com/rss/search?q=" + searchterm + "&hl=en-CA&gl=CA&ceid=CA%3Aen"
         self.links = set()
         self.art_list = []
-        self.number = number
         self.GoogleSearch()
         
 
@@ -39,10 +39,12 @@ class News:
         Client.close()
         soup_page=soup(xml_page,"xml")
         self.news_list=soup_page.findAll("item")
+    
+    def links(self):
         for each in self.news_list:
-            if len(self.links)<self.number:
-                self.links.add(each.link.text)
-
+            self.links.add(each.link.text)
+    
+    def article_list(self):
         for link in self.links:
             art = Article(link)
             try:
@@ -52,3 +54,13 @@ class News:
                 pass
             finally:
                 self.art_list.append(art)
+                
+    def count_words(self):
+        wordcounts = np.array([len(each.text.split()) for each in self.art_list])
+        return wordcounts
+        
+    def keywords(self):
+        keywords = [each.keywords for each in self.art_list]     
+        return keywords 
+    
+            
